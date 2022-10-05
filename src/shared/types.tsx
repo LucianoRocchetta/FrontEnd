c5afdcf37c1959a0ea86debb8b04444204973d39
chocolule <fideos.12.11@gmail.com>
2022-10-04 23:54:15 -0300

    new carousel component, new custom components, and presets

--- src/shared/types.tsx
+++ src/shared/types.tsx
@@ -2,33 +2,40 @@ import { CSSProperties } from "react";
 
 // API SERVICE TYPES
 export interface BaseApiParams {
-    url: string;
-    unauthorizedCallback?: any;
-    showSuccessMessage?: boolean;
+  url: string;
+  unauthorizedCallback?: any;
+  showSuccessMessage?: boolean;
 }
 
 export interface GetParams extends BaseApiParams {}
 
 export interface ReprocessData {
-    method: "GET" | "POST" | "PATCH" | "DELETE";
-    body: any;
-    url: string;
+  method: "GET" | "POST" | "PATCH" | "DELETE";
+  body: any;
+  url: string;
 }
 
 // COMPONENTS PROPS
 export interface NavbarProps {
-    currentScreen: ScreenNames;
+  currentScreen: ScreenNames;
 }
 export interface GenericButtonProps {
-    children?: any;
-    onClick?: (e: any) => void;
-    className?: string;
-    style?: CSSProperties;
+  preset?: "play_button" | "info_button";
+  disabled?: boolean;
+  children?: any;
+  onClick?: (e: any) => void;
+  className?: string;
+  style?: CSSProperties;
+}
+export interface ArrowButtonProps {
+  hasNext: boolean;
+  clickHandler: () => void;
+  direction: "prev" | "next";
 }
 
 // TYPES
 export enum ScreenNames {
-    HOME = "Home",
-    GENRES = "Genres",
-    MYLIST = "MyList",
+  HOME = "Home",
+  GENRES = "Genres",
+  MYLIST = "MyList",
 }
