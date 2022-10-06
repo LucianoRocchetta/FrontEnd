afedcf28dabefe7bf2f6e5d4ab25cfa5c80f9151
LucianoRocchetta <lucianorocchetta@gmail.com>
2022-10-05 21:33:18 -0300

    movies-grid and cards

--- src/components/Layout/Body/components/MovieGrid/index.tsx
+++ src/components/Layout/Body/components/MovieGrid/index.tsx
@@ -2,21 +2,18 @@ import { useState } from "react";
 import { Card } from "../../../../common";
 import { MoviesGridProps } from "../../../../../shared/types";
 
-export const MoviesGrid = ({movies:{category, images}}:MoviesGridProps) => {
-    const [movies, setMovies] = useState(images);
-    /*TEST*/
-    return (
-        
-        <div className = "movies-grid">
-            <h2>{category}</h2>
-            <div className = 'grid-container'>
-            {
-                movies.map((movie:string, i:any) => {
-                    console.log(movie);
-                    return <Card img = {movie} key = {i}/>
-                })
-            }
-            </div>
-        </div>
-    )
-}
+export const MoviesGrid = ({
+  movies: { category, images },
+}: MoviesGridProps) => {
+  const [movies, setMovies] = useState(images);
+  return (
+    <div className="movies-grid">
+      <h2>{category}</h2>
+      <div className="grid-container">
+        {movies.map((movie, i) => (
+          <Card img={movie} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/Layout/Body/index.tsx
+++ src/components/Layout/Body/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { MoviesGrid } from "./components/MovieGrid";
 
 export const Body = () => {
@@ -12,9 +12,9 @@ export const Body = () => {
         "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/950/public/media/image/2016/11/matrix.jpg?itok=y_D1-7f-",
         "https://es.web.img3.acsta.net/medias/nmedia/18/67/06/38/20350558.jpg",
         "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/poster-peliculas-terror-2019-prodigy-1578395572.jpg?crop=1xw:1xh;center,top&resize=480:*",
-        "https://i.pinimg.com/474x/4a/a7/09/4aa709fb827464c325ee3c63f07b8a6c.jpg"  
-      ]
-    }, 
+        "https://i.pinimg.com/474x/4a/a7/09/4aa709fb827464c325ee3c63f07b8a6c.jpg",
+      ],
+    },
     {
       category: "comedy",
       images: [
@@ -23,18 +23,15 @@ export const Body = () => {
         "https://image.tmdb.org/t/p/original/qJIggHPX3uil110CjO8ITPpYroH.jpg",
         "https://wwwimage-us.pplusstatic.com/thumbnails/photos/w370-q80/movie_asset/92/68/77/PPL_SAlone_Poster_1400x2100_.jpg",
         "https://es.web.img3.acsta.net/medias/nmedia/18/95/76/36/20426888.jpg",
-        "https://play-lh.googleusercontent.com/0ajUa-474t1aRsGY2Z791IcXimECPsZOwp6WpdbcTdnbnnj13tgbB_nivUq9j8JNVknz"
-      ]
-    }
-  ])
-  return (  
+        "https://play-lh.googleusercontent.com/0ajUa-474t1aRsGY2Z791IcXimECPsZOwp6WpdbcTdnbnnj13tgbB_nivUq9j8JNVknz",
+      ],
+    },
+  ]);
+  return (
     <div>
-      {
-        categories.map((data) => {
-          return <MoviesGrid movies = {data}/>
-        })
-      }
-      
+      {categories.map((data, i) => (
+        <MoviesGrid movies={data} key={i} />
+      ))}
     </div>
-    );
+  );
 };
