6c92ca26f19423acfcc99cc35d30e44d9cc43364
Matrox1821 <matiasgg1821@gmail.com>
2022-10-13 18:46:51 -0300

    new layout, navbar improved and body fixes

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,3 +1,12 @@
-export * from "./Navbar";
-export * from "./Body";
-export * from "./Carousel";
+import React from "react";
+import { LayoutProps } from "../../shared/types";
+import { Navbar } from "./Navbar";
+
+export const Layout = (props: LayoutProps) => {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <main>{props.children}</main>
+    </div>
+  );
+};
